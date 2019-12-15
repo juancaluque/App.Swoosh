d7558dcafc73f9077892b80e4368a95ec4765e2e
Jukess <juancaluque@hotmail.es>
2019-12-15 10:16:38 -0600

    lesson-09 done

---
+++ app.swoosh/app.swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app.swoosh
+//
+//  Created by Juan Luque on 12/15/19.
+//  Copyright © 2019 Juan Luque. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkilLevel: String?
+    
+}
